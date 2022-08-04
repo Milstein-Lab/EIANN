eb8aee95dcb14401f593955c7e306a1751cfc3b3
argalloni <alessandro.galloni.11@ucl.ac.uk>
2022-08-04 02:51:31 -0400

    Updated classes to streamline object/class handling + made class objects iterable + added forward() function

--- universalNet.py
+++ universalNet.py
@@ -23,58 +23,51 @@ params_dict = {'layer_0':
 
 model = universalNet(params_dict)
 '''
-'''
-hparams = {'tau': 1,
-           'seed': 42}
 
-params_dict = {'layer_0':
-                   {'E':
-                        {'n': 7,
-                         'projections': {}}},
-               'layer_1':
-                   {'E':
-                        {'n': 100,
-                         'bias': False,
-                         'activation': 'relu',
-                         'projections': 
-                             {'layer_0':{'E':[]}}}},
-               'layer_2':
-                   {'E':
-                        {'n': 10,
-                         'bias': False,
-                         'activation': 'relu',
-                         'projections':
-                             {'layer_1':{'E':[]}}}}
-               }
-'''
+
+activation_dict = {'linear': lambda x: x,
+                   'relu': nn.ReLU(),
+                   'sigmoid': nn.Sigmoid(),
+                   'softplus': nn.Softplus(beta=4)}
 
 
 class universalNet(nn.Module):
     def __init__(self):
         super().__init__()
+        self.tau = 1
+        self.seed = 42
+        self.nn_modules = nn.ModuleList()
+        self.net = Net()
 
-        self.input_layer = Layer(populations={'E':7, 'I':1})
+        self.net.input_layer = Layer('input_layer', populations={'E':10})
 
-        self.layer_1 = Layer(populations={'E':7, 'I':1})
-        self.layer_1.E.activation = 'softplus'
+        self.net.layer_1 = Layer('layer_1', populations={'E':7, 'I':2})
+        self.net.layer_1.E.activation = 'softplus'
+        self.net.layer_1.E.add_projections(self, [self.net.input_layer.E])
 
-        self.layer_2 = Layer(populations={'E':5})
-        self.layer_2.E.activation = 'softplus'
+        self.net.layer_2 = Layer('layer_2', populations={'E':5})
+        self.net.layer_2.E.activation = 'softplus'
+        self.net.layer_2.E.add_projections(self, [self.net.input_layer.E, self.net.layer_1.E])
 
-        self.layer_2.E.add_projections([self.input_layer.E, self.layer_1.E])
-        # self.layer_2.E.add_projections(['input_layer.E', 'layer_1.E'])
+    def forward(self, input_pattern):
+        for i, layer in enumerate(self.net):
+            for j, population in enumerate(layer):
+                if i==0 and j==0: # set activity for the first population of the input layer
+                    population.activity = input_pattern
+
+                delta_state = -population.state
+                for projection in population:
+                    delta_state += projection(projection.pre.activity)
+                population.state += delta_state / self.tau
+                population.activity = population.activation(population.state)
 
 
-#
-#
 # class universalNet(nn.Module):
 #     def __init__(self, params_dict, hparams):
 #         super().__init__()
 #         self.tau = hparams['tau']
 #         self.seed = hparams['seed']
 #
-#         # self.net = nn.Sequential()
-#         # self.net.add_module('name',nn.Linear)
 #         # self.nn_modules = nn.ModuleList()
 #
 #         self.net = Net()
@@ -92,61 +85,95 @@ class universalNet(nn.Module):
 #                         pre_size = params_dict[incoming_layer][pre_population]['n']
 #                         self.net.__dict__[layer].__dict__[population].__dict__[incoming_layer] = Projection(pre_size, post_size)
 #
-#
-#     def forward(self, x):
-#         for layer in self.net:
-#             return
 
 
 class Net(object):
     def __iter__(self):
-        for value in self.__dict__.values():
+        for key,value in self.__dict__.items():
             yield value
+
+    def __repr__(self):
+        items = ", ".join([name for name in self.__dict__])
+        return f'{type(self)} :\n\t({items})'
 
 
 class Layer(nn.Module):
-    def __init__(self, populations):
+    def __init__(self, name, populations):
         super().__init__()
+        self.name = name
         self.populations = populations
         for pop,size in populations.items():
-            self.__dict__[pop] = Population(size)
+            self.__dict__[pop] = Population(layer=self.name, name=pop, size=size)
+
+    def __iter__(self):
+        for key,value in self.__dict__.items():
+            if callable(value): #only iterate over Populations
+                yield value
 
 
 class Population(nn.Module):
-    def __init__(self, size):
+    def __init__(self, layer, name, size):
         super().__init__()
+
+        # Hyperparameters
+        self.layer = layer
+        self.name = name
+        self.fullname = self.layer + self.name
         self.size = size
         self.activation = 'linear'
-        self.bias_rule = None
-        self.bias = torch.zeros(size)
+        self.bias_rule = 'backprop'
         self.learn_bias = False
+        self.inputs = []
 
+        # State variables
+        self.state = torch.zeros(self.size)
         self.activity = torch.zeros(self.size)
+        self.bias = torch.zeros(size)
+
         self.prev_activity = torch.zeros(self.size)
         self.activity_history = None
-        self.state = torch.zeros(self.size)
-        self.projections = {}
 
-    def add_projections(self, pre_populations):
-        self.inputs = pre_populations
+    def add_projections(self, network, pre_populations):
         for pre_pop in pre_populations:
-            self.__dict__[pre_pop] = nn.Linear(pre_pop.size, self.size, bias=False)
+            name = pre_pop.layer + pre_pop.name
+            self.inputs.append(name)
+            self.__dict__[name] = nn.Linear(pre_pop.size, self.size, bias=False)
+
+            # Set projection attributes
+            projection = self.__dict__[name]
+            projection.name = f'{name}_to_{self.fullname}'
+            projection.pre = pre_pop
+            projection.learning_rule = 'backprop'
+            projection.direction = 'FF'
+            projection.lr = 0.01
+
+            # Add to ModuleList to make projection a trainable parameter
+            network.nn_modules.append(projection)
+
+    def __iter__(self):
+        for key,value in self.__dict__.items():
+            if isinstance(value, nn.Linear): #only iterate over projections
+                yield value
+
+    # Automatically turn activation string into a callable function
+    @property
+    def activation(self):
+        return activation_dict[self._act_name]
+
+    @activation.setter
+    def activation(self, act_name):
+        self._act_name = act_name
+
 
 
 # class universalNet1(nn.Module):
 #     def __init__(self, params_dict):
 #         super().__init__()
 #         self.params_dict = params_dict
-#
 #         self.nn_modules = nn.ModuleList()
-#         self.net = nn.Sequential()
 #
 #         self.input_layer = Layer(n=7,)
-#
-#
-#
-#         self.net.add_module()
-#
+##
 #         for layer in params_dict:
 #             self.net.layer = Layer(param_args)
 #
