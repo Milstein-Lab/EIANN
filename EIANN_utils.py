f945b354f3073c0d3982529391abc790b1813ae1
Aaron D. Milstein <neurosutras@gmail.com>
2022-08-12 17:14:37 -0400

    20220812 - EIANN implementation in progress.

--- EIANN_utils.py
+++ EIANN_utils.py
@@ -380,3 +380,31 @@ def backprop_backward(network, output, target):
     network.optimizer.zero_grad()
     loss.backward()
 
+
+def get_diag_argmax_row_indexes(data):
+    """
+    Sort the rows of a square matrix such that whenever row argmax and col argmax are equal, that value appears
+    on the diagonal. Returns row indexes.
+    :param data: 2d array; square matrix
+    :return: array of int
+    """
+    data = np.array(data)
+    if data.shape[0] != data.shape[1]:
+        raise Exception('get_diag_argmax_row_indexes: data must be a square matrix')
+    dim = data.shape[0]
+    avail_row_indexes = list(range(dim))
+    avail_col_indexes = list(range(dim))
+    final_row_indexes = np.empty_like(avail_row_indexes)
+    while(len(avail_col_indexes) > 0):
+        row_selectivity = np.zeros_like(avail_row_indexes)
+        row_max = np.max(data[avail_row_indexes, :][:, avail_col_indexes], axis=1)
+        row_mean = np.mean(data[avail_row_indexes,:][:,avail_col_indexes], axis=1)
+        nonzero_indexes = np.where(row_mean > 0)
+        row_selectivity[nonzero_indexes] = row_max[nonzero_indexes] / row_mean[nonzero_indexes]
+
+        row_index = avail_row_indexes[np.argsort(row_selectivity)[-1]]
+        col_index = avail_col_indexes[np.argmax(data[row_index,avail_col_indexes])]
+        final_row_indexes[col_index] = row_index
+        avail_row_indexes.remove(row_index)
+        avail_col_indexes.remove(col_index)
+    return final_row_indexes
