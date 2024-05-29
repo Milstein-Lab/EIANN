74f47d994de96369fd1d7196fd83729adb30057b
argalloni <alessandro.galloni.11@ucl.ac.uk>
2024-05-29 15:28:06 -0400

    updated tests

--- tests/test_build_network.py
+++ tests/test_build_network.py
@@ -3,6 +3,7 @@ import torch
 import numpy as np
 
 import matplotlib.pyplot as plt
+
 import EIANN.utils as ut
 import EIANN.plot as pt
 import EIANN._network as nt
@@ -16,20 +17,22 @@ def test_get_MNIST_dataloaders():
     assert isinstance(data_generator, torch.Generator)
 
 def test_build_network():
-    config_dir = "../EIANN/network_config/mnist/"
+    root_dir = ut.get_project_root()
+    config_dir = "/network_config/mnist/"
     config_name = "20231120_EIANN_1_hidden_mnist_van_bp_relu_SGD_config_G_optimized.yaml"
-    test_config = config_dir + config_name
+    test_config = root_dir + config_dir + config_name
     bpDale_network = ut.build_EIANN_from_config(test_config, network_seed=66049)
     assert isinstance(bpDale_network, nt.Network)
 
 def test_load_network():
-    config_dir = "../EIANN/network_config/mnist/"
+    root_dir = ut.get_project_root()
+    config_dir = "/network_config/mnist/"
     config_name = "20231120_EIANN_1_hidden_mnist_van_bp_relu_SGD_config_G_optimized.yaml"
-    test_config = config_dir + config_name
+    test_config = root_dir + config_dir + config_name
 
-    saved_network_dir = "../EIANN/data/mnist/"
+    saved_network_dir = "/data/mnist/"
     saved_network = "20231120_EIANN_1_hidden_mnist_van_bp_relu_SGD_config_G_66049_257.pkl"
-    saved_network_path = saved_network_dir+saved_network
+    saved_network_path = root_dir + saved_network_dir + saved_network
 
     bpDale_network = ut.build_EIANN_from_config(test_config, network_seed=66049)
     bpDale_network.load(saved_network_path)
