177d12065186f566ef55939a9e4319a8c7a31414
argalloni <alessandro.galloni.11@ucl.ac.uk>
2024-05-30 17:02:06 -0400

    updated tests to run through all config files

--- tests/test_build_network.py
+++ tests/test_build_network.py
@@ -1,20 +1,12 @@
 import pytest
 import torch
-import numpy as np
-
-import matplotlib.pyplot as plt
+import os
 
 import EIANN.utils as ut
 import EIANN.plot as pt
 import EIANN._network as nt
 
-@pytest.fixture(scope="module")
-def network():
-    root_dir = ut.get_project_root()
-    config_dir = "/EIANN/network_config/mnist/"
-    config_name = "20231120_EIANN_1_hidden_mnist_van_bp_relu_SGD_config_G_optimized.yaml"
-    test_config = root_dir + config_dir + config_name
-    return ut.build_EIANN_from_config(test_config, network_seed=66049)
+
 
 @pytest.fixture(scope="module")
 def MNIST_dataloaders():
@@ -26,6 +18,33 @@ def MNIST_dataloaders():
     assert isinstance(data_generator, torch.Generator)
     return train_dataloader, train_sub_dataloader, val_dataloader, test_dataloader, data_generator
 
+@pytest.fixture(scope="module")
+def root_dir():
+    root_dir = ut.get_project_root()
+    return root_dir
+
+@pytest.fixture(scope="module")
+def network(root_dir):
+    config_dir = "/EIANN/network_config/mnist/"
+    config_name = "20231120_EIANN_1_hidden_mnist_van_bp_relu_SGD_config_G_optimized.yaml"
+    test_config = root_dir + config_dir + config_name
+    network = ut.build_EIANN_from_config(test_config, network_seed=66049)
+    return network
+
+
+def test_build_network(root_dir):
+    config_dir = root_dir + "/EIANN/network_config/mnist/"
+    assert len(os.listdir(config_dir)) > 0, "No network configs found in the network config dir"
+    assert all([(config_name.endswith(".yaml") or config_name.endswith(".yml")) for config_name in os.listdir(config_dir)]), "Not all files in the network config dir are yaml files"
+
+    for config_name in os.listdir(config_dir):
+        try:
+            network = ut.build_EIANN_from_config(config_dir + config_name, network_seed=66049)
+        except:
+            raise Exception(f"Failed to build network from config: {config_name}")
+        assert isinstance(network, nt.Network)
+
+
 def test_train_network(network, MNIST_dataloaders):
     train_dataloader, train_sub_dataloader, val_dataloader, test_dataloader, data_generator =  MNIST_dataloaders
     network.train(train_sub_dataloader, 
@@ -35,19 +54,11 @@ def test_train_network(network, MNIST_dataloaders):
                   store_history=True, 
                   store_params=True,
                   status_bar=True)
+    
 
-# def test_load_network():
-#     root_dir = ut.get_project_root()
-#     config_dir = "/EIANN/network_config/mnist/"
-#     config_name = "20231120_EIANN_1_hidden_mnist_van_bp_relu_SGD_config_G_optimized.yaml"
-#     test_config = root_dir + config_dir + config_name
-
-#     saved_network_dir = "/EIANN/data/mnist/"
-#     saved_network = "20231120_EIANN_1_hidden_mnist_van_bp_relu_SGD_config_G_66049_257.pkl"
-#     saved_network_path = root_dir + saved_network_dir + saved_network
-
-#     print(test_config)
-#     print(saved_network_path)
-
-#     bpDale_network = ut.build_EIANN_from_config(test_config, network_seed=66049)
-#     bpDale_network.load(saved_network_path)
+@pytest.mark.skipif(os.environ.get("CI") == "true", reason="Pickle file not saved in GitHub Actions")
+def test_load_network(network, root_dir):
+    saved_network_dir = root_dir + "/EIANN/data/mnist/"
+    saved_network = "20231120_EIANN_1_hidden_mnist_van_bp_relu_SGD_config_G_66049_257.pkl"
+    saved_network_path = saved_network_dir + saved_network
+    network.load(saved_network_path)
