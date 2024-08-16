0707d667779964a6cb63b1e2fe9ea73051aec3d1
argalloni <alessandro.galloni.11@ucl.ac.uk>
2024-08-16 14:50:05 -0400

    simplified submodule __init__

--- EIANN/rules/__init__.py
+++ EIANN/rules/__init__.py
@@ -2,18 +2,10 @@
 This module contains various learning rules for updating weights in neural networks.
 """
 
-from .base_classes import LearningRule, BiasLearningRule
-from .backprop import Backprop, BackpropBias, Backprop_EWC, Backprop_DendriticLoss
+from .base_classes import *
+from .backprop import *
 from .backprop_like import *
 from .hebbian import *
 from .btsp import *
 from .dendritic_loss import *
 from .weight_functions import *
-
-# __all__ = [
-#     'LearningRule', 'BiasLearningRule',
-#     'Backprop', 'BackpropBias', 'Backprop_EWC', 'Backprop_DendriticLoss',
-#     'HebbianRule', 'OjaRule', 'Hebb_WeightNorm',
-#     'DendriticLoss', 'DendriticLossBias',
-#     'clone_weight', 'normalize_weight', 'no_autapses',
-# ]
