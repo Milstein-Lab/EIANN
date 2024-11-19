90df72e83ab0c011b0324829b32058a5099977e5
Yash Chennawar <144838733+yashc73080@users.noreply.github.com>
2024-11-19 10:06:30 -0500

    Updated dend_EI_contrast learned_bias hyperparameters

--- EIANN/plot.py
+++ EIANN/plot.py
@@ -1721,6 +1721,9 @@ def plot_FB_weight_alignment(*projections, title=None):
 def plot_spiral_accuracy(net, test_dataloader):
     '''
     Function to plot loss landscape of spiral classification task by marking incorrect points red
+
+    :param net: network object after training
+    :param test_dataloader: dataloader with (data,target) to use for computing loss.
     '''
     fig, axes = plt.subplots(1, 1, figsize=(5, 5))
 
