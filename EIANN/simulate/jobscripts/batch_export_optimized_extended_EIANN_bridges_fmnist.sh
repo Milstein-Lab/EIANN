31ec2e09a485a52f5b4a7ec516c6f4a71fbc25c9
Aaron D. Milstein <neurosutras@gmail.com>
2025-08-21 16:15:23 -0400

    20250821:   - Exporting fmnist networks after extended training.

---
+++ EIANN/simulate/jobscripts/batch_export_optimized_extended_EIANN_bridges_fmnist.sh
@@ -0,0 +1,35 @@
+#SBATCH -J batch_export_optimized_extended_EIANN_fmnist
+#SBATCH -o /ocean/projects/bio240068p/aaronmil/logs/EIANN/batch_export_optimized_extended_EIANN_fmnist.%j.o
+#SBATCH -e /ocean/projects/bio240068p/aaronmil/logs/EIANN/batch_export_optimized_extended_EIANN_fmnist.%j.e
+#SBATCH -p RM
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=12
+#SBATCH -n 12
+#SBATCH -t 6:00:00
+#SBATCH -A bio250022p
+#SBATCH --mail-user=milstein@cabm.rutgers.edu
+#SBATCH --mail-type=ALL
+
+cd $WORK/EIANN/EIANN/simulate
+
+export CONFIG_DIR=../network_config/fmnist
+
+export MPI4PY_RC_RECV_MPROBE=false
+
+declare -a config_files=(
+  20250819_EIANN_0_hidden_fmnist_van_bp_relu_SGD_config_G_zero_bias_optimized.yaml
+  20250819_EIANN_2_fixed_hidden_fmnist_van_bp_relu_SGD_config_G_zero_bias_optimized.yaml
+)
+
+arraylength=${#config_files[@]}
+
+declare o=0
+for ((i=0; i<${arraylength}; i++))
+do
+  mpirun -n 6 -o $o python -m mpi4py.futures simulate_EIANN_fashion_mnist.py \
+    --network-config-file-path=$CONFIG_DIR/${config_files[$i]} \
+    --output-dir=../data/fmnist --disp --label=extended --export \
+    --framework=mpi &
+  ((o+=6))
+done
+wait
