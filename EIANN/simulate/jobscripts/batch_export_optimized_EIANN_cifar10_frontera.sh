a39a4396ded85f85044dc8b464da60fe1c98ae11
Aaron Milstein <neurosutras@gmail.com>
2025-09-22 12:26:42 -0500

    20250922 - updating Frontera jobscripts

--- EIANN/simulate/jobscripts/batch_export_optimized_EIANN_cifar10_frontera.sh
+++ EIANN/simulate/jobscripts/batch_export_optimized_EIANN_cifar10_frontera.sh
@@ -28,7 +28,7 @@ arraylength=${#config_files[@]}
 declare o=0
 for ((i=0; i<${arraylength}; i++))
 do
-  ibrun -n 6 -o $o python -m mpi4py.futures simulate_EIANN_fashion_cifar10.py \
+  ibrun -n 6 -o $o python -m mpi4py.futures simulate_EIANN_cifar10.py \
     --network-config-file-path=$CONFIG_DIR/${config_files[$i]} \
     --output-dir=$SCRATCH/data/EIANN --disp --export \
     --framework=mpi &
