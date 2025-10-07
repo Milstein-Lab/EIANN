f2a776221f865be78d2b7572461a7760313f49c5
Aaron D. Milstein <neurosutras@gmail.com>
2025-10-07 14:38:53 -0400

    20251007:   - Exporting training data for Supervised_Hebb_WeightNorm on Frontera.

---
+++ EIANN/simulate/jobscripts/batch_export_optimized_EIANN_mnist_frontera.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+#SBATCH -J batch_export_optimized_EIANN_mnist
+#SBATCH -o /scratch1/06441/aaronmil/logs/EIANN/batch_export_optimized_EIANN_mnist.%j.o
+#SBATCH -e /scratch1/06441/aaronmil/logs/EIANN/batch_export_optimized_EIANN_mnist.%j.e
+#SBATCH -p normal
+#SBATCH -N 1
+#SBATCH -n 18
+#SBATCH -t 2:00:00
+#SBATCH --mail-user=milstein@cabm.rutgers.edu
+#SBATCH --mail-type=ALL
+
+set -x
+
+cd $WORK/EIANN/EIANN/simulate
+
+export CONFIG_DIR=../network_config/mnist
+
+export MPI4PY_RC_RECV_MPROBE=false
+
+declare -a config_files=(
+  20240714_EIANN_2_hidden_mnist_Supervised_Hebb_WeightNorm_config_4_optimized.yaml
+)
+
+arraylength=${#config_files[@]}
+
+declare o=0
+for ((i=0; i<${arraylength}; i++))
+do
+  ibrun -n 6 -o $o python -m mpi4py.futures simulate_EIANN_mnist.py \
+    --network-config-file-path=$CONFIG_DIR/${config_files[$i]} \
+    --output-dir=$SCRATCH/data/EIANN --disp --export \
+    --framework=mpi &
+  ((o+=6))
+done
+wait
