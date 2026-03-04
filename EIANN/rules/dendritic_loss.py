fbd6a305aad5ea9ab36375e35789891d3c6a70c7
Yash Chennawar <yash.chennawar@gmail.com>
2026-03-04 10:50:08 -0500

    Fixed use_amp bug

--- EIANN/rules/dendritic_loss.py
+++ EIANN/rules/dendritic_loss.py
@@ -5,13 +5,7 @@ import torch
 class DendriticLossBias(BiasLearningRule):
     def step(self):
         # self.population.bias.data += self.learning_rate * self.population.dendritic_state
-        bias_update += self.learning_rate * self.population.plateau
-
-        # Convert to same dtype as bias for AMP compatibility
-        if self.population.network.use_amp and bias_update.dtype != self.population.bias.dtype:
-            bias_update = bias_update.to(self.population.bias.dtype)
-
-        self.population.bias.data += bias_update
+        self.population.bias.data += self.learning_rate * self.population.plateau
 
 
 class DendriticLoss_6(LearningRule):
@@ -31,9 +25,5 @@ class DendriticLoss_6(LearningRule):
             delta_weight = torch.outer(
                 torch.clamp(self.projection.post.forward_dendritic_state.detach().clone(), min=-1, max=1),
                 torch.clamp(self.projection.pre.forward_prev_activity, min=0, max=1))
-            
-        # Ensure delta_weight has same dtype as weight for AMP compatibility
-        if self.projection.post.network.use_amp and delta_weight.dtype != self.projection.weight.dtype:
-            delta_weight = delta_weight.to(self.projection.weight.dtype)
         
         self.projection.weight.data += self.sign * self.learning_rate * delta_weight
