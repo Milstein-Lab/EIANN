39e22e46da15ab8ac464d0c2580619fb432f052c
Yash Chennawar <yash.chennawar@gmail.com>
2026-04-01 11:12:02 -0400

    Fixed resource allocation, improved ray script

--- EIANN/optimize/jobscripts/optimize_EIANN_ray_frontera_mnist.sh
+++ EIANN/optimize/jobscripts/optimize_EIANN_ray_frontera_mnist.sh
@@ -7,15 +7,18 @@
 #SBATCH --ntasks=1
 #SBATCH --partition=rtx
 #SBATCH --mem=80G
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=8
 #SBATCH --time=03:00:00
 #SBATCH --mail-user=yc1376@scarletmail.rutgers.edu
 #SBATCH --mail-type=ALL
 
+set -euo pipefail
+set -x
+
 mkdir -p $SCRATCH/logs/EIANN
 mkdir -p $SCRATCH/data/EIANN
 
-export OMP_NUM_THREADS=4
+export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
@@ -32,14 +35,27 @@ export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
 cd $HOME/EIANN/EIANN
 
-# Start Ray (local mode, 4 GPUs per RTX node)
-ray start --head --num-gpus=4 --num-cpus=16
+export RAY_TMPDIR="$SCRATCH/ray/${SLURM_JOB_ID}"
+mkdir -p $RAY_TMPDIR
+
+ray start --head --port=6379 \
+  --num-cpus=8 --num-gpus=4 \
+  --temp-dir "$RAY_TMPDIR" --disable-usage-stats
+
+cleanup_ray() {
+  set +e
+  ray stop --force || true
+}
+trap cleanup_ray EXIT
 
 python -m nested.optimize --config-file-path=$1 \
   --output-dir=$SCRATCH/data/EIANN --framework=ray --disp \
-  --pop_size=15 --max_iter=15 --path_length=3
+  --pop_size=4 --max_iter=2 --path_length=2 --num_gpus=0.5 --num_cpus=1
 
-ray stop --force || true
+# num models tested = pop_size * num_seeds (5)
+# num generations = max_iter * path_length
+# Ray workers = min(floor(total_gpus/num_gpus), floor(total_cpus/num_cpus))
+# must match num_cpus, num_gpus in ray start to the SBATCH lines
 
 # cd $HOME/EIANN/EIANN/optimize/jobscripts
 # sbatch optimize_EIANN_ray_frontera_mnist.sh optimize/optimize_config/mnist/20231129_nested_optimize_EIANN_2_hidden_mnist_van_bp_relu_SGD_config_G.yaml
@@ -53,5 +69,17 @@ ray stop --force || true
 
 
 
-#######
-# 
+# -----------------------------------------------------
+# van_bp_0_hidden: 
+# ray: 
+#   - 4 generations took 232.09 s (7613482)
+#   - 45 generations took 8466.89 s (7613450)
+# mpi: 
+#   - 4 generations took 458.27 s (7613483)
+
+# van_bp_2_hidden:
+# ray:
+#   - 4 generations took 680.48 s (7626273)
+# mpi:
+#   - 4 generations took 1110.16 s
+
--- EIANN/optimize/jobscripts/optimize_MPI_EIANN_frontera_MNIST.sh
+++ EIANN/optimize/jobscripts/optimize_MPI_EIANN_frontera_MNIST.sh
@@ -10,7 +10,7 @@ sbatch <<EOT
 #SBATCH -e /scratch2/11358/yashchennawar5555/logs/EIANN/$JOB_NAME.%j.e
 #SBATCH -p development
 #SBATCH -N 1
-#SBATCH -n 8
+#SBATCH -n 21
 #SBATCH -t 2:00:00
 #SBATCH --mail-user=yc1376@scarletmail.rutgers.edu
 #SBATCH --mail-type=ALL
@@ -25,9 +25,15 @@ cd $HOME/EIANN/EIANN
 
 export MPI4PY_RC_RECV_MPROBE=false
 
-ibrun -n 8 python -m mpi4py.futures -m nested.optimize --config-file-path=$CONFIG_FILE_PATH \
-  --output-dir=$SCRATCH/data/EIANN --pop_size=15 --max_iter=15 --path_length=3 --disp \
-  --framework=mpi
+ibrun -n 21 python -m mpi4py.futures -m nested.optimize --config-file-path=$CONFIG_FILE_PATH \
+  --output-dir=$SCRATCH/data/EIANN --framework=mpi --disp \
+  --pop_size=4 --max_iter=2 --path_length=2 
 EOT
 
+# -n: num procs = 1 master + pop_size * num_seeds (5)
+# num generations = max_iter * path_length
+
 # ./optimize_MPI_EIANN_frontera_MNIST.sh optimize/optimize_config/mnist/20231129_nested_optimize_EIANN_2_hidden_mnist_van_bp_relu_SGD_config_G.yaml van_bp
+
+
+# TODO move to $WORK
